dea3697784d33ef11397451f5cfdb18b7a2be5b6
Arty <amakrtumyan@mail.ru>
2021-11-22 12:01:42 +0300

    mapping test version

--- Domain/Picture.cs
+++ Domain/Picture.cs
@@ -4,7 +4,6 @@
     using System.Collections.Generic;
     using Staff.Extensions;
 
-
     /// <summary>
     /// Картина.
     /// </summary>
--- PictureGallery/Program.cs
+++ PictureGallery/Program.cs
@@ -1,16 +1,26 @@
 ﻿namespace PictureGallery
 {
+    using NHibernate;
     using System;
-
-    internal class Program
+    using System.Reflection;
+    public class Program
     {
         private static void Main(string[] args)
         {
             var author = new Domain.Author(1, "Васнецов", "Виктор");
             var picture = new Domain.Picture(1, "Богатыри");
             author.AddPicture(picture);
-            
+
             Console.WriteLine($"{picture} {author}");
+
+            var sessionFactory = ORM.NHibernateConfigurator.GetSessionFactory(showSQL: true);
+
+            using(var session = sessionFactory.OpenSession())
+            {
+                session.Save(author);
+                session.Save(picture);
+                session.Flush();
+            }
         }
     }
 }
